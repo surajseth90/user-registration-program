3b9bd8d17afafb641360c8c4e6917ac0eccb3a4c
surajseth90 <surajseth90@gmail.com>
2021-04-29 20:49:07 +0530

    refactor code for use case 6

--- src/main/java/com/bridgelabz/userregistration/Password.java
+++ src/main/java/com/bridgelabz/userregistration/Password.java
@@ -2,7 +2,7 @@ package com.bridgelabz.userregistration;
 import java.util.Scanner;
 
 public class Password {
-	private static final String passwordCondition = "(?=.*[A-Z])[A-Za-z]{8,}";
+	private static final String passwordCondition = "(?=.*[A-Z])[A-Za-z]{7,}";
 		
 	public static String validPassword() {
 		Scanner scanner = new Scanner(System.in);
@@ -12,7 +12,8 @@ public class Password {
 		if(CheckCondition.checkCondition(password,passwordCondition))
 			System.out.println();
 		else {
-			System.out.println("Invalid Input : Please Enter in a proper format ,eg. minimum 8 characters ");
+			System.out.println("Invalid Input : Please Enter in a proper format "+"\n"
+					+ "eg. minimum 8 characters and altleast 1 upper case character ");
 			Password.validPassword();
 		}
 		return password;
